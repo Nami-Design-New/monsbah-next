c45414ff3e45b22afc9e2a0fa0fb0907787616c9
Ahmed-Sallam22 <ahmed30sallam22@gmail.com>
2025-11-09 14:46:41 +0200

    handel build phase 2

---
+++ src/app/[country-locale]/test-error/page.jsx
@@ -0,0 +1,10 @@
+'use client';
+
+export const dynamic = 'force-dynamic';
+
+export default function TestErrorPage() {
+  // This will throw an error intentionally
+  throw new Error('This is a test error to see the error page!');
+  
+  return <div>You should not see this</div>;
+}
