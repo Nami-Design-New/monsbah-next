f9d6d901fe4b47da412738657fab2f4447c7bf47
Ahmed-Sallam22 <ahmed30sallam22@gmail.com>
2025-11-15 17:24:37 +0200

    handel error 500

--- src/app/[country-locale]/test-error/page.jsx
+++
@@ -1,7 +0,0 @@
-// Force dynamic rendering
-export const dynamic = 'force-dynamic';
-
-export default function TestErrorPage() {
-  // This will throw an error intentionally
-  throw new Error('This is a test error to see the error page!');
-}
