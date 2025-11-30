0ea1aa4bfc5f26e7a18feb002891c8b870fa13f1
Ahmed-Sallam22 <ahmed30sallam22@gmail.com>
2025-11-30 14:42:27 +0200

    update icon

--- src/app/[country-locale]/layout.jsx
+++ src/app/[country-locale]/layout.jsx
@@ -87,12 +87,14 @@ export async function generateMetadata({ params }) {
     },
     icons: {
       icon: [
-        { url: "/storeicon.svg", sizes: "any" },
-        { url: "/storeicon.svg", sizes: "192x192", type: "image/png" },
-        { url: "/storeicon.svg", sizes: "512x512", type: "image/png" },
+        { url: "/storeicon.svg", sizes: "any", type: "image/svg+xml" },
+        { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+        { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       ],
       apple: [
-        { url: "/storeicon.svg", sizes: "180x180", type: "image/png" },
+        { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
       ],
       other: [
         { rel: "mask-icon", url: "/branding/icon.svg", color: "#00CCBB" },
